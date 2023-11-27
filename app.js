68bd9cf99c14bf1c769931f96e1c3c974671a091
Veera Hiltunen <veera.hiltunen@gmail.com>
2023-11-27 12:53:08 +0200

    Create app.js that will be imported in index.js

---
+++ app.js
@@ -0,0 +1,45 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,46 +1,6 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
-
+const app = require('./app.js')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-
-
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
 
 app.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
