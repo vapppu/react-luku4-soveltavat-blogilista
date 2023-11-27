7a7143fcc3d164a82e2903d65d280cc6ad313649
Veera Hiltunen <veera.hiltunen@gmail.com>
2023-11-27 13:53:42 +0200

    Create controller for blog requests and model for Blog database

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,2 +1,30 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+blogsRouter.post('/', (request, response, next) => {
+    console.log("Post request")
+    const body = request.body
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    })
+
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+        .catch(error => next(error))
+})
+
+module.exports = blogsRouter
