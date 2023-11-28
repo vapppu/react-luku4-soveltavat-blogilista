82c2c36b424dce4ba816ebe948badf94123fc181
Veera Hiltunen <veera.hiltunen@gmail.com>
2023-11-28 13:59:00 +0200

    Add function and unit tests for finding the blog with most likes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,16 @@ const totalLikes = (blogs) => {
         , 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const likes = (blogs.map(blog => Number(blog.likes)))
+    const mostLikes = Math.max(...likes)
+    const blogi = blogs.find(blog => blog.likes === mostLikes)
+    console.log(blogi)
+    return blogi
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
