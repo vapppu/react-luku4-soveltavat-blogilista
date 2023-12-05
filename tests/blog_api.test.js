74e95e846f70e99690d4f0a2b438cf4d4ddfb12e
Veera Hiltunen <veera.hiltunen@gmail.com>
2023-12-05 15:49:20 +0200

    Ex. 4.8: create tests for GET request at /api/blogs: test that a correct amount of blogs is returned as JSON

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,40 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+        "title": "Blogi",
+        "author": "Veera",
+        "url": "www.blogi.com",
+        "likes": 6,
+        "id": "6564719721e99569fde868af"
+    },
+    {
+        "title": "Heppablogi",
+        "author": "Liisa",
+        "url": "www.heppablogini.fi",
+        "likes": 348,
+        "id": "656472af21e99569fde868b1"
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObjects = initialBlogs
+        .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+
+    // KORJAA TÄHÄN ETTÄ SUORITETAAN KOKO TAULUKOLLE
+})
+
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length)
+})
 
 test('notes are returned as json', async () => {
     await api
