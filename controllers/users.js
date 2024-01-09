b195b4f2cec724cf068c03402325b6e3813e2689
Veera Hiltunen <veera.hiltunen@gmail.com>
2024-01-09 15:30:46 +0200

    Populate user and note tables so that user's blogs and blog's user are shown in get requests

--- controllers/users.js
+++ controllers/users.js
@@ -36,7 +36,7 @@ usersRouter.post('/', async (request, response, next) => {
 })
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User.find({}).populate('blogs', { url: 1, title: 1, author: 1 })
     response.json(users)
 })
 
--- models/user.js
+++ models/user.js
@@ -10,6 +10,12 @@ const userSchema = mongoose.Schema({
     },
     name: String,
     passwordHash: String,
+    blogs: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Blog'
+        }
+    ]
 })
 
 userSchema.plugin(uniqueValidator)
